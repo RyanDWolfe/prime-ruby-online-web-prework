175a951d1db2eddcf6c6529a6da962ca3b4c0b22
Ryan Wolfe <ryan@studiohopfitness.com>
2019-02-08 20:45:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def prime?(number)
     return false if number < 2
 
@@ -9,3 +10,23 @@ def prime?(number)
     end
     true
   end
+=======
+def prime?(x)
+  f = 2
+  r = []
+
+  while x % f == 0
+    r << f
+    f += 1
+  end
+
+  puts r
+# need to store loop final value for 'f'
+
+#  if r == x
+#    true
+#  else
+#    false
+#  end
+end
+>>>>>>> d52d3b0e099231bfd8bf76ea6ccf7cef7712306e
